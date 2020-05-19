0da6f4fdbcde32d7056188b1f0d959cd1fde7823
Keegan McCafferty <kmmccafferty96@gmail.com>
2020-05-18 23:38:47 -0400

    feat(theme): add flb theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
