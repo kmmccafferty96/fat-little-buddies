664c04774fa5c1030c1a953738005abff1d63afe
Keegan McCafferty <kmmccafferty96@gmail.com>
2020-05-21 00:20:00 -0400

    feat(nav): add sidenav and toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [],
-  imports: [FontAwesomeModule],
-  exports: [FontAwesomeModule]
+  imports: [MatSidenavModule, MatToolbarModule, MatButtonModule, FontAwesomeModule],
+  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, FontAwesomeModule]
 })
 export class CoreModule {
   constructor(private iconLibrary: FaIconLibrary) {
-    iconLibrary.addIcons(faFacebook, faInstagram, faTwitter);
+    iconLibrary.addIcons(faBars, faFacebook, faInstagram, faTwitter);
   }
 }
