7ac5b418c511bcdd8e8391e5c27a00a0869c061f
Keegan McCafferty <kmmccafferty96@gmail.com>
2021-06-12 00:29:54 -0400

    feat(home): add carousel

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { HomeView } from './home.view';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CoreModule } from '../../core/core.module';
 
 @NgModule({
-  declarations: [HomeView],
-  imports: [CommonModule, RouterModule.forChild([{ path: '', component: HomeView }])]
+  declarations: [HomeView, CarouselComponent],
+  imports: [CommonModule, RouterModule.forChild([{ path: '', component: HomeView }]), CoreModule]
 })
 export class HomeModule {}
--- src/app/views/home/home.view.ts
+++ src/app/views/home/home.view.ts
@@ -1,8 +1,41 @@
 import { Component } from '@angular/core';
 
+import { CarouselItem } from './components/carousel/carousel.component';
+
 @Component({
   selector: 'flb-home',
   templateUrl: './home.view.html',
   styleUrls: ['./home.view.scss']
 })
-export class HomeView {}
+export class HomeView {
+  carouselItems: CarouselItem[] = [
+    {
+      title: 'Great Food',
+      description: `Here is the description. It's a good description.`,
+      buttonText: 'View Menu',
+      buttonUrl: 'menu',
+      imgSrc: 'assets/wings.jpg'
+    },
+    {
+      title: 'Buffetfest',
+      description: `Here is the description. It's a good description.`,
+      buttonText: 'Buy Tickets',
+      buttonUrl: 'events',
+      imgSrc: 'assets/parrot.jpg'
+    },
+    {
+      title: 'Buffetfest',
+      description: `Here is the description. It's a good description.`,
+      buttonText: 'Buy Tickets',
+      buttonUrl: 'events',
+      imgSrc: 'assets/parrot2.jpg'
+    },
+    {
+      title: 'Buffetfest',
+      description: `Here is the description. It's a good description.`,
+      buttonText: 'Buy Tickets',
+      buttonUrl: 'events',
+      imgSrc: 'assets/parrot3.jpg'
+    }
+  ];
+}
