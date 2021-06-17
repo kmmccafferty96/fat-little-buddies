c611675de8c9516c6d6b37fb52e45a927383f594
Keegan McCafferty <kmmccafferty96@gmail.com>
2021-06-17 17:03:19 -0400

    feat: add firebase and github actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDgQF5Z4-WnUz-jFP0rQRAvw0DJo2PGj_U',
+    authDomain: 'fat-little-buddies.firebaseapp.com',
+    projectId: 'fat-little-buddies',
+    storageBucket: 'fat-little-buddies.appspot.com',
+    messagingSenderId: '1065652648949',
+    appId: '1:1065652648949:web:ab9425e086d81ff56ebebc',
+    measurementId: 'G-M17S4WF1V9'
+  }
 };
