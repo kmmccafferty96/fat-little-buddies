b7fba2ef1616f2e7de6095258bc7dd0d4c778a73
Keegan McCafferty <kmmccafferty96@gmail.com>
2021-06-16 23:25:35 -0400

    feat(sidenav): implement sidenav

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [],
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatDividerModule, FontAwesomeModule]
+  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatDividerModule, MatListModule, FontAwesomeModule]
 })
 export class CoreModule {
   constructor(private iconLibrary: FaIconLibrary) {
